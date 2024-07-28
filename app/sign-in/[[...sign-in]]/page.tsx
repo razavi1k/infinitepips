5591928d35e724ded50fc86a4d87cbeae7da7abe
razavi1k <razavi1k@gmail.com>
2024-07-28 20:24:11 +0330

    first commit

---
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation"
+import { SignIn, useUser } from "@clerk/nextjs";
+
+const SignInPage = () => {
+    return (
+        <div className={`w-full h-screen flex justify-center items-center`}>
+            <SignIn />
+        </div>
+    );
+}
+export default SignInPage;
